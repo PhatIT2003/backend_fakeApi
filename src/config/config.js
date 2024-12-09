0a103c91bb3e2c10f3fc2093190552a5aca0ab39
PhatIT2003 <banconcop113@gmail.com>
2024-12-09 09:15:43 +0700

    a..

--- src/config/config.js
+++ src/config/config.js
@@ -3,8 +3,8 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-    SERVER_PORT: 3001,
-    DELAY_TIMEOUT: 0, //unit: milliseconds
+    SERVER_PORT: 4000,
+    DELAY_TIMEOUT: 500, //unit: milliseconds
     JWT_SECRET: process.env.JWT_Token,
     TELEGRAM_BOT_TOKEN: process.env.PRIVATE_KEY,
     FRONTEND_URL: 'http://localhost:3000'
